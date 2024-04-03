6fbc738a09ef8c6850295ca92a4e8a750fd0db84
Thiago Leite <thiagoleites@gmail.com>
2024-04-03 00:43:00 -0300

    refactor: changes routes list

--- routes/api.php
+++ routes/api.php
@@ -9,4 +9,8 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 
-Route::resource('tasks', TaskController::class);
+Route::get('tasks', [TaskController::class, 'index']);
+Route::post('tasks', [TaskController::class, 'store']);
+Route::get('tasks/{id}', [TaskController::class, 'show']);
+Route::put('tasks/{id}', [TaskController::class, 'update']);
+Route::delete('tasks/{id}', [TaskController::class, 'destroy']);
