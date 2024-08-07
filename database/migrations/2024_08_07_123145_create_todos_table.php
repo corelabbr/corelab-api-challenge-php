e8864a9d41eb25720121c671ac152f636cd6c4b8
Felipe Carvalho <felipe_pc7@hotmail.com>
2024-08-07 10:57:21 -0300

    fix title column name

--- database/migrations/2024_08_07_123145_create_todos_table.php
+++ database/migrations/2024_08_07_123145_create_todos_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('todos', function (Blueprint $table) {
             $table->id();
-            $table->string('tite');
+            $table->string('title');
             $table->string('description');
             $table->string('color')->default('white');
             $table->boolean('favorite')->default(false);
