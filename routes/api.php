97b96d2bb79912814863fb4af8e2dc4e941cec88
Felipe Carvalho <felipe_pc7@hotmail.com>
2024-08-07 21:44:25 -0300

    add "todos" prefix to routes

--- routes/api.php
+++ routes/api.php
@@ -4,11 +4,13 @@ use App\Http\Controllers\api\TodosController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/todos', [TodosController::class, 'index']);
-Route::get('/todos/favorites', [TodosController::class, 'getFavorites']);
-Route::get('/todos/others', [TodosController::class, 'getOthersTodos']);
-Route::post('/todos/create', [TodosController::class, 'store']);
-Route::patch('/todos/{todo}/update', [TodosController::class, 'update']);
-Route::patch('/todos/{todo}/update/color', [TodosController::class, 'setTodoColor']);
-Route::patch('/todos/{todo}/update/favorite', [TodosController::class, 'favoriteTodo']);
-Route::delete('/todos/{todo}/delete', [TodosController::class, 'delete']);
+Route::prefix('todos')->group(function () {
+    Route::get('/', [TodosController::class, 'index']);
+    Route::get('/favorites', [TodosController::class, 'getFavorites']);
+    Route::get('/others', [TodosController::class, 'getOthersTodos']);
+    Route::post('/create', [TodosController::class, 'store']);
+    Route::patch('/{todo}/update', [TodosController::class, 'update']);
+    Route::patch('/{todo}/update/color', [TodosController::class, 'setTodoColor']);
+    Route::patch('/{todo}/update/favorite', [TodosController::class, 'favoriteTodo']);
+    Route::delete('/{todo}/delete', [TodosController::class, 'delete']);
+});
