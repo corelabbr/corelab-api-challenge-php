ee256235e45f0889b2cdffcc8c321ba263634ae8
GiovaniAppezzato <giovani.apztt@gmail.com>
2024-08-12 09:56:39 -0300

    feat: added NoteTest

---
+++ database/factories/NoteFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Note;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Note>
+ */
+class NoteFactory extends Factory
+{
+    protected $model = Note::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->sentence,
+            'content' => $this->faker->paragraph,
+            'is_favorite' => $this->faker->boolean,
+        ];
+    }
+}
